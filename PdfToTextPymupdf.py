12affa2de304938d0e262dff6b51f47f91d539b4
dtucker1876 <148928437+dtucker1876@users.noreply.github.com>
2024-07-05 12:01:29 -0400

    initial

---
+++ PdfToTextPymupdf.py
@@ -0,0 +1,11 @@
+import fitz  # PyMuPDF
+
+doc = fitz.open("a.pdf")  # open a document
+out = open("output.txt", "wb")  # create a text output
+
+for page in doc:  # iterate the document pages
+    text = page.get_text("text").encode("utf8")  # get plain text (is in UTF-8)
+    out.write(text)  # write text of page
+    out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
+
+out.close()
